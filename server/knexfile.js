73d2663273dbc5d7e0d528e0f47f2650b4c36186
Sagar Singh <richsagarsingh@gmail.com>
2025-02-05 12:50:40 -0800

    chore: cleanup

--- server/knexfile.js
+++ server/knexfile.js
@@ -26,6 +26,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
+      host: process.env.PGHOST,
       database: process.env.PGDATABASE,
       user:     process.env.PGUSER,
       password: process.env.PGPASSWORD
